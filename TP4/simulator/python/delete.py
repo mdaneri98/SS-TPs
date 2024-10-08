77c4fb1c5e9f549702336124b0a3a92466a74c5e
Matias <mdaneri98@gmail.com>
2024-10-08 15:54:21 -0300

    Fixed parameter to run with

--- TP4/simulator/python/delete.py
+++ TP4/simulator/python/delete.py
@@ -2,34 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parámetros
-b = 100.0  # Constante de amortiguamiento
-k = 100000.0  # Constante del resorte
-mass = 70.0  # Masa
-max_time = 5.0  # Tiempo máximo
-timestep = 1.0E-5  # Paso de tiempo
-initial_amplitude = 1.0  # Amplitud inicial
-initial_position = 1.0  # Posición inicial
-initial_velocity = -0.7142857142857143  # Velocidad inicial
+A_inicial = 1  # Amplitud inicial en metros
+posicion_inicial = 1  # Posición inicial en metros
+velocidad_inicial = posicion_inicial  # Velocidad inicial (igual a la posición inicial)
+masa = 70  # Masa en kg
+k = 10**4  # Constante del resorte en N/m
+b = 100  # Coeficiente de amortiguamiento en kg/s
+tf = 5  # Tiempo final en segundos
+dt = 0.001  # Incremento de tiempo
 
-# Calcular la frecuencia angular amortiguada
-omega_d = np.sqrt(k / mass - (b / (2 * mass)) ** 2)
+# Función de aceleración derivada de la ecuación diferencial
+def aceleracion(x, v, m, b, k):
+    return -(b / m) * v - (k / m) * x
 
-# Tiempo
-times = np.arange(0, max_time, timestep)
+# Variables para almacenar tiempo, posición y velocidad
+t = np.arange(0, tf, dt)
+x = np.zeros_like(t)
+v = np.zeros_like(t)
 
-# Cálculo de la posición x(t)
-# Fase inicial (phi) calculada a partir de la posición y velocidad iniciales
-phi = np.arctan((initial_velocity + (b / (2 * mass)) * initial_position) / (omega_d * initial_amplitude))
-positions = initial_amplitude * np.exp(-b / (2 * mass) * times) * np.cos(omega_d * times + phi)
+# Condiciones iniciales
+x[0] = posicion_inicial
+v[0] = velocidad_inicial
 
-# Crear el gráfico
-plt.figure(figsize=(12, 6))
-plt.plot(times, positions, label='Oscilador Armónico Amortiguado', color='blue')
-plt.title('Movimiento de un Oscilador Armónico Amortiguado')
+# Método de Euler para resolver la ecuación diferencial
+for i in range(1, len(t)):
+    a = aceleracion(x[i-1], v[i-1], masa, b, k)
+    v[i] = v[i-1] + a * dt
+    x[i] = x[i-1] + v[i-1] * dt
+
+# Gráfica de posición en función del tiempo
+plt.figure(figsize=(10, 6))
+plt.plot(t, x, label='Posición (m)')
+plt.title('Oscilación Amortiguada: Posición en función del tiempo')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Posición (m)')
-plt.xlim(0, max_time)
-plt.ylim(min(positions) * 1.1, max(positions) * 1.1)
-plt.grid()
+plt.grid(True)
 plt.legend()
 plt.show()
--- TP4/simulator/src/Main.java
+++ TP4/simulator/src/Main.java
@@ -9,21 +9,21 @@ public class Main {
     public static void main(String[] args) {
 
 
-        OscillatorSystem os = new OscillatorSystem(100, 10e4, 70, 5,  1);
+        OscillatorSystem os = new OscillatorSystem(100, 1e4, 70, 5,  1);
 
-        double[] timesteps = new double[]{0.00001, 0.00001, 0.0001, 0.001, 0.01, 0.1};
+        double[] timesteps = new double[]{1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1};
         for (Double timestep : timesteps) {
             os.analiticSolution(timestep);
             os.verletSolution(timestep);
             os.beemanSolution(timestep);
         }
 
-        CoupledOscillatorSystem cos = new CoupledOscillatorSystem(100, 100, 0.001, 60, 10e-3, 10e-2);
-        cos.verletSolution(10, 10e-3);
+        CoupledOscillatorSystem cos = new CoupledOscillatorSystem(100, 100, 0.001, 60, 1e-3, 1e-2);
+        cos.verletSolution(10, 1e-3);
         double[] wfs = new double[15];
         for (int i = 5; i < wfs.length; i++) {
             wfs[i] = i;
-            cos.verletSolution(wfs[i],10e-3);
+            cos.verletSolution(wfs[i],1e-3);
         }
 
 
